8592c333757474121278e326d208af85b6f78f72
Marcos Maio <120725100+MarcosMaio@users.noreply.github.com>
2023-10-13 13:03:20 -0300

    att img to Image tag

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useSelectedCharacter } from '@/src/context/SelectedCharacter';
 import { useCharacters } from '@/src/context/Characters';
+import Image from 'next/image';
 
 
 interface Character {
@@ -32,7 +33,7 @@ export default function HomePage() {
             {showBeginCharacter && (
                 <div key={showBeginCharacter.name} className='bg-cover bg-no-repeat bg-right-top h-screen flex justify-evenly items-center' style={{ backgroundImage: `url(${showBeginCharacter.image})` }}>
                     <div className='text-white pl-20 flex flex-col text-left'>
-                        <img className='w-full h-full max-w-customLogo mb-4 slideAnimation delay-500' src='/assets/One Piece Logo Vector.svg' alt="" />
+                        <Image className='max-w-customLogo mb-4 slideAnimation delay-500' src='/assets/One Piece Logo Vector.svg' alt="" width='250' height='250' />
                         <h1 className='mb-4 text-T-1 font-bold tracking-L-Space-1 slideAnimation delay-1000'>{showBeginCharacter.name}</h1>
                         <p className='w-[35%] text-P-1 leading-L-Height-1 slideAnimation delay-[1500ms]'>{showBeginCharacter.description}</p>
                     </div>
