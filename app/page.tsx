329bc0bb44580cdf8bb0b80444dee8d2a7a38dad
Marcos Maio <120725100+MarcosMaio@users.noreply.github.com>
2023-10-14 12:01:23 -0300

    att favicon and title

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import SideBar from "@/src/components/SideBar";
 import { CharactersProvider } from "@/src/context/Characters";
 import { SelectedCharacterProvider } from "@/src/context/SelectedCharacter";
 import API from "@/src/hooks/use_API";
+import '../styles/globals.css';
+import '../styles/tailwind.css';
 
 import { QueryClient, QueryClientProvider } from 'react-query';
 
